7cbaf4efb15d1e158157125f7ca09f48221113ce
McDonnough <psbaechli+atlassian@gmail.com>
2017-11-22 14:45:06 +0100

    add business-layer methods

--- AutoReservation.BusinessLayer/ReservationManager.cs
+++ AutoReservation.BusinessLayer/ReservationManager.cs
@@ -4,11 +4,77 @@ using System.Collections.Generic;
 using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
 using System.Linq;
+using AutoReservation.Dal.Entities;
 
 namespace AutoReservation.BusinessLayer
 {
     public class ReservationManager
         : ManagerBase
     {
+        public List<Reservation> List
+        {
+            get
+            {
+                using (var context = new AutoReservationContext())
+                {
+                    return context.Reservationen.ToList();
+                }
+            }
+        }
+
+        public Kunde Find(int id)
+        {
+            using (var context = new AutoReservationContext())
+            {
+                return context.Kunden.Find(id);
+            }
+        }
+
+        public void Add(Reservation reservation)
+        {
+            using (var context = new AutoReservationContext())
+            {
+                // TODO Constraints einbauen
+
+                context.Entry(reservation).State = EntityState.Added;
+                context.Reservationen.Add(reservation);
+                context.SaveChanges();
+            }
+        }
+
+        public void Update(Reservation reservation)
+        {
+            using (var context = new AutoReservationContext())
+            {
+                try
+                {
+                    // TODO Constraints einbauen
+
+                    context.Entry(reservation).State = EntityState.Modified;
+                    context.SaveChanges();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    throw CreateOptimisticConcurrencyException<Reservation>(context, reservation);
+                }
+            }
+        }
+
+        public void Delete(Reservation reservation)
+        {
+            using (var context = new AutoReservationContext())
+            {
+                try
+                {
+                    context.Entry(reservation).State = EntityState.Deleted;
+                    context.Reservationen.Remove(reservation);
+                    context.SaveChanges();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    throw CreateOptimisticConcurrencyException<Reservation>(context, reservation);
+                }
+            }
+        }
     }
 }
