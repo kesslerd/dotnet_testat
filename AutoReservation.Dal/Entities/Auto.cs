4d0e2fa7ea1f02b60345346698a0623fe22fde74
kesslerd <dominik@trueworld.ch>
2017-11-22 09:36:12 +0100

    add auto classes

--- AutoReservation.Dal/Entities/Auto.cs
+++ AutoReservation.Dal/Entities/Auto.cs
@@ -4,5 +4,19 @@ using System.Data.Entity;
 
 namespace AutoReservation.Dal.Entities
 {
- 
+    public class Auto
+    {
+        public int Id { get; set; }
+        public string Marke { get; set; }
+        public byte[] RowVersion { get; set; }
+        public int Tagestarif { get; set; }
+    }
+
+    public class StandardAuto : Auto { }
+
+    public class LuxusklasseAuto : Auto {
+        public int Basistarif { get; set; }
+    }
+
+    public class MittelklasseAuto : Auto { }
 }
