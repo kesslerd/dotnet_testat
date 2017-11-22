b0084f7f8b1b14c23978511e56612a6f1c852804
kesslerd <dominik@trueworld.ch>
2017-11-22 11:25:07 +0100

    add KundenManager

--- AutoReservation.BusinessLayer/KundeManager.cs
+++ AutoReservation.BusinessLayer/KundeManager.cs
@@ -4,11 +4,78 @@ using System.Collections.Generic;
 using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
 using System.Linq;
+using AutoReservation.Dal.Entities;
 
 namespace AutoReservation.BusinessLayer
 {
     public class KundeManager
         : ManagerBase
     {
+        public List<Kunde> List
+        {
+            get
+            {
+                using (var context = new AutoReservationContext())
+                {
+                    return context.Kunden.ToList();
+                }
+            }
+        }
+
+        public Kunde Find(int id)
+        {
+            using (var context = new AutoReservationContext())
+            {
+                return context.Kunden.Find(id);
+            }
+        }
+
+        public void Add(Kunde kunde)
+        {
+            using (var context = new AutoReservationContext())
+            {
+                context.Entry(kunde).State = EntityState.Added;
+                context.Kunden.Add(kunde);
+                context.SaveChanges();
+            }
+        }
+
+        public void Update(Kunde kunde)
+        {
+            using (var context = new AutoReservationContext())
+            {
+                var entity = context.Kunden.Find(kunde.Id);
+                if (entity == null)
+                {
+                    return;
+                }
+
+                try
+                {
+                    context.Entry(entity).CurrentValues.SetValues(kunde);
+                    context.SaveChanges();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    throw CreateOptimisticConcurrencyException<Kunde>(context, kunde);
+                }
+            }
+        }
+
+        public void Delete(Kunde kunde)
+        {
+            using (var context = new AutoReservationContext())
+            {
+                try
+                {
+                    context.Kunden.Remove(kunde);
+                    context.SaveChanges();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    throw CreateOptimisticConcurrencyException<Kunde>(context, kunde);
+                }
+            }
+        }
     }
 }
