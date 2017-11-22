c6afec421258e644ff849d54a772098938cbe1b2
kesslerd <dominik@trueworld.ch>
2017-11-22 09:42:16 +0100

    create kunde

--- AutoReservation.Dal/Entities/Kunde.cs
+++ AutoReservation.Dal/Entities/Kunde.cs
@@ -5,5 +5,12 @@ using System.Data.Entity;
 
 namespace AutoReservation.Dal.Entities
 {
-  
+    public class Kunde
+    {
+        public DateTime Geburtsdatum { get; set; }
+        public int Id { get; set; }
+        public string Nachname { get; set; }
+        public string Vorname { get; set; }
+        public byte[] RowVersion { get; set; }
+    }
 }
