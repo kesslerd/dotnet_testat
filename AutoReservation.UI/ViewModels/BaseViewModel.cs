a9743fb5bbeeb0a6ecaf5e67bc416d3fc89474a0
kesslerd <kesslerd@users.noreply.github.com>
2017-11-29 09:31:34 +0100

    add BaseViewModel (#36)
    * add BaseViewModel
    
    * refactor

---
+++ AutoReservation.UI/ViewModels/BaseViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoReservation.UI.ViewModels
+{
+    class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected bool SetProperty<T>(ref T field, T value, [CallerMemberName] string name = null, , params string[] otherNames)
+        {
+            if (Equals(field, value))
+                return false;
+
+            field = value;
+
+            OnPropertyChanged(name);
+            foreach (var n in otherNames)
+            {
+                OnPropertyChanged(n);
+            }
+
+            return true;
+        }
+
+        protected void OnPropertyChanged(string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
