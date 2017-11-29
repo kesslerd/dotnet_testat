694211f12cbd423f0380a12836ca28958200542e
kesslerd <dominik@trueworld.ch>
2017-11-29 14:44:40 +0100

    implement Kunde WCF interface

--- AutoReservation.Common/Interfaces/IAutoReservationService.cs
+++ AutoReservation.Common/Interfaces/IAutoReservationService.cs
@@ -1,8 +1,16 @@
 ﻿using AutoReservation.Common.DataTransferObjects;
+using System.Collections.Generic;
 
 namespace AutoReservation.Common.Interfaces
 {
     public interface IAutoReservationService
     {
+        #region Kunde
+        KundeDto GetKunde(int id);
+        List<KundeDto> GetKunden();
+        void UpdateKunde(KundeDto kunde);
+        void AddKunde(KundeDto kunde);
+        void DeleteKunde(KundeDto kunde);
+        #endregion
     }
 }
--- AutoReservation.Service.Wcf/AutoReservationService.cs
+++ AutoReservation.Service.Wcf/AutoReservationService.cs
@@ -1,12 +1,45 @@
 ﻿using System;
 using System.Diagnostics;
+using AutoReservation.Common.DataTransferObjects;
+using AutoReservation.Common.Interfaces;
+using AutoReservation.BusinessLayer;
+using System.Collections.Generic;
 
 namespace AutoReservation.Service.Wcf
 {
-    public class AutoReservationService
+    public class AutoReservationService : IAutoReservationService
     {
+        KundeManager kundeManager = new KundeManager();
 
         private static void WriteActualMethod() 
             => Console.WriteLine($"Calling: {new StackTrace().GetFrame(1).GetMethod().Name}");
+
+        public void AddKunde(KundeDto kunde)
+        {
+            var kundeEntity = DtoConverter.ConvertToEntity(kunde);
+            kundeManager.Add(kundeEntity);
+        }
+
+        public void DeleteKunde(KundeDto kunde)
+        {
+            var kundeEntity = DtoConverter.ConvertToEntity(kunde);
+            kundeManager.Delete(kundeEntity);
+        }
+
+        public KundeDto GetKunde(int id)
+        {
+            return DtoConverter.ConvertToDto(kundeManager.Find(id));
+        }
+
+        public List<KundeDto> GetKunden()
+        {
+            return DtoConverter.ConvertToDtos(kundeManager.List);
+        }
+
+        public void UpdateKunde(KundeDto kunde)
+        {
+            var kundeEntity = DtoConverter.ConvertToEntity(kunde);
+            kundeManager.Update(kundeEntity);
+        }
     }
 }
