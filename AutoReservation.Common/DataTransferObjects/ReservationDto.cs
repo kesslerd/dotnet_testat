ba4e304d64983eacfc6f5ed4ed6a4b6e3baf254c
rpfister102 <remo.pfister@hsr.ch>
2017-12-03 17:39:47 +0100

    Added ReservationDTO

--- AutoReservation.Common/DataTransferObjects/ReservationDto.cs
+++ AutoReservation.Common/DataTransferObjects/ReservationDto.cs
@@ -6,7 +6,14 @@ namespace AutoReservation.Common.DataTransferObjects
     public class ReservationDto
     {
 
-        //public override string ToString()
-        //    => $"{ReservationsNr}; {Von}; {Bis}; {Auto}; {Kunde}";
+        public int ReservationsNr { get; set; }
+        public DateTime Von { get; set; }
+        public DateTime Bis { get; set; }
+        public AutoDto Auto { get; set; }
+        public KundeDto Kunde { get; set; }
+        public byte[] ReservationsVersion { get; set; }
+
+        public override string ToString()
+            => $"{ReservationsNr}; {Von}; {Bis}; {Auto}; {Kunde}";
     }
 }
