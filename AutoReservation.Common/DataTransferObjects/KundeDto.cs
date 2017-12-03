f6a61ef4af6f6d065ccb9aa26a751e09a045f4be
kesslerd <dominik@trueworld.ch>
2017-12-03 19:16:14 +0100

    add deco

--- AutoReservation.Common/DataTransferObjects/KundeDto.cs
+++ AutoReservation.Common/DataTransferObjects/KundeDto.cs
@@ -3,13 +3,19 @@ using System.Runtime.Serialization;
 
 namespace AutoReservation.Common.DataTransferObjects
 {
+    [DataContract]
     public class KundeDto
     {
 
-      public int Id { get; set; }
+        [DataMember]
+        public int Id { get; set; }
+        [DataMember]
         public string Nachname { get; set; }
+        [DataMember]
         public string Vorname { get; set; }
+        [DataMember]
         public DateTime Geburtsdatum { get; set; }
+        [DataMember]
         public byte[] RowVersion { get; set; }
 
         public override string ToString()
