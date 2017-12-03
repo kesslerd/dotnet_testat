364aa7a11264bb802c0d86a97fb85d5b5af22e6f
rpfister102 <remo.pfister@hsr.ch>
2017-12-03 17:18:23 +0100

    Added Tests

--- AutoReservation.BusinessLayer.Testing/AutoUpdateTest.cs
+++ AutoReservation.BusinessLayer.Testing/AutoUpdateTest.cs
@@ -11,6 +11,9 @@ namespace AutoReservation.BusinessLayer.Testing
         private AutoManager target;
         private AutoManager Target => target ?? (target = new AutoManager());
 
+        private int STANDARD_AUTO_ID = 1;
+        private int NOT_EXISTING_AUTO_ID = 99;
+
 
         [TestInitialize]
         public void InitializeTestData()
@@ -19,9 +22,44 @@ namespace AutoReservation.BusinessLayer.Testing
         }
 
         [TestMethod]
+        public void FindAutoTest()
+        {
+            Auto auto = Target.Find(STANDARD_AUTO_ID);
+            Assert.AreEqual(auto.Id, STANDARD_AUTO_ID);
+
+        }
+
+        [TestMethod]
         public void UpdateAutoTest()
         {
-            Assert.Inconclusive("Test not implemented.");
+            String marke = "BMW";
+            Auto auto = Target.Find(STANDARD_AUTO_ID);
+            auto.Marke = marke;
+            Target.Update(auto);
+            Auto updatedAuto = Target.Find(STANDARD_AUTO_ID);
+            Assert.AreEqual(updatedAuto.Marke, marke);
+        }
+
+        [TestMethod]
+        public void UpdateNonExistingAuto()
+        {
+            Target.Update(new Auto() { Id = NOT_EXISTING_AUTO_ID });
+            Auto updatedAuto = Target.Find(NOT_EXISTING_AUTO_ID);
+            Assert.AreEqual(updatedAuto, null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exceptions.OptimisticConcurrencyException<Auto>))]
+        public void UpdateAutoOptimisticConcurrencyTest()
+        {
+            Auto a1 = Target.Find(STANDARD_AUTO_ID);
+            Auto a2 = Target.Find(STANDARD_AUTO_ID);
+
+            a1.Marke = "BMW";
+            a2.Marke = "Audi";
+
+            Target.Update(a1);
+            Target.Update(a2);
         }
     }
 }
