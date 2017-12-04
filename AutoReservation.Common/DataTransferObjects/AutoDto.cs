78bce643a6499754367e71cd9a8d019f10c8138c
rpfister102 <remo.pfister@hsr.ch>
2017-12-04 14:18:01 +0100

    Added Annotations

--- AutoReservation.Common/DataTransferObjects/AutoDto.cs
+++ AutoReservation.Common/DataTransferObjects/AutoDto.cs
@@ -3,14 +3,25 @@ using System.Runtime.Serialization;
 
 namespace AutoReservation.Common.DataTransferObjects
 {
+    [DataContract]
     public class AutoDto
     {
-
+        [DataMember]
         public int Id { get; set; }
+
+        [DataMember]
         public string Marke { get; set; }
+
+        [DataMember]
         public int Tagestarif { get; set; }
+
+        [DataMember]
         public int Basistarif { get; set; }
+
+        [DataMember]
         public AutoKlasse AutoKlasse { get; set; }
+
+        [DataMember]
         public byte[] RowVersion { get; set; }
 
         public override string ToString()
