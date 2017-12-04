680d5f09b80d7d7507435c5f65d6901149bcd140
kesslerd <kesslerd@users.noreply.github.com>
2017-12-04 14:09:22 +0100

    [WIP] kunde ui (#40)
    * create view
    
    * add UI
    
    * implement baseviewmodel
    
    * refactor
    
    * fix ef
    
    * add viewmodel
    
    * add context

---
+++ AutoReservation.UI/ViewModels/KundeViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoReservation.Common.DataTransferObjects;
+
+namespace AutoReservation.UI.ViewModels
+{
+    public class KundeViewModel : BaseViewModel
+    {
+
+        private int _id;
+
+        public int Id
+        {
+            get { return _id; }
+            set { SetProperty(ref _id, value, nameof(Id)); }
+        }
+
+
+        private String _nachname;
+
+        public String Nachname
+        {
+            get { return _nachname; }
+            set { SetProperty(ref _nachname, value, nameof(Nachname)); }
+        }
+
+        private String _vorname;
+
+        public String Vorname
+        {
+            get { return _vorname; }
+            set { SetProperty(ref _vorname, value, nameof(Vorname)); }
+        }
+
+
+        private DateTime _geburtsdatum;
+
+        public DateTime Geburtsdatum
+        {
+            get { return _geburtsdatum; }
+            set { SetProperty(ref _geburtsdatum, value, nameof(Geburtsdatum)); }
+        }
+
+        private byte[] _rowVersion;
+
+        public byte[] RowVerwion
+        {
+            get { return _rowVersion; }
+            set { SetProperty(ref _rowVersion, value, nameof(RowVerwion)); }
+        }
+    }
+}
---
+++ AutoReservation.UI/Views/Kunde.xaml.cs
@@ -0,0 +1,36 @@
+﻿using AutoReservation.UI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AutoReservation.UI.Views
+{
+
+    /// <summary>
+    /// Interaction logic for Kunde.xaml
+    /// </summary>
+    public partial class Kunde : Window
+    {
+        public KundeViewModel ViewModel { get; set; }
+
+
+        public Kunde()
+        {
+            InitializeComponent();
+
+            ViewModel = new KundeViewModel();
+
+            DataContext = this;
+        }
+    }
+}
