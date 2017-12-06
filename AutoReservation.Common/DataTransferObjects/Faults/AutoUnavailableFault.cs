b8a3ca7f2216cd193dda6eb42efe406dd624ce93
rpfister102 <remo.pfister@hsr.ch>
2017-12-06 10:05:28 +0100

    added fault contracts

---
+++ AutoReservation.Common/DataTransferObjects/Faults/AutoUnavailableFault.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoReservation.Common.DataTransferObjects.Faults
+{
+    class AutoUnavailableFault
+    {
+        public string Message{ get; set; }
+    }
+}
--- AutoReservation.Common/Interfaces/IAutoReservationService.cs
+++ AutoReservation.Common/Interfaces/IAutoReservationService.cs
@@ -1,4 +1,5 @@
 ﻿using AutoReservation.Common.DataTransferObjects;
+using AutoReservation.Common.DataTransferObjects.Faults;
 using System;
 using System.Collections.Generic;
 using System.ServiceModel;
@@ -16,12 +17,14 @@ namespace AutoReservation.Common.Interfaces
         List<KundeDto> GetKunden();
 
         [OperationContract]
+        [FaultContract(typeof(DataManipulationFault))]
         void UpdateKunde(KundeDto kunde);
 
         [OperationContract]
         void AddKunde(KundeDto kunde);
 
         [OperationContract]
+        [FaultContract(typeof(DataManipulationFault))]
         void DeleteKunde(KundeDto kunde);
         #endregion
 
@@ -37,12 +40,14 @@ namespace AutoReservation.Common.Interfaces
         List<AutoDto> GetAutos();
 
         [OperationContract]
+        [FaultContract(typeof(DataManipulationFault))]
         void UpdateAuto(AutoDto auto);
 
         [OperationContract]
         void AddAuto(AutoDto auto);
 
         [OperationContract]
+        [FaultContract(typeof(DataManipulationFault))]
         void DeleteAuto(AutoDto auto);
         #endregion
 
@@ -55,12 +60,18 @@ namespace AutoReservation.Common.Interfaces
         List<ReservationDto> GetReservations();
 
         [OperationContract]
+        [FaultContract(typeof(DataManipulationFault))]
+        [FaultContract(typeof(InvalidDateRangeFault))]
+        [FaultContract(typeof(AutoUnavailableFault))]
         void UpdateReservation(ReservationDto reservation);
 
         [OperationContract]
+        [FaultContract(typeof(InvalidDateRangeFault))]
+        [FaultContract(typeof(AutoUnavailableFault))]
         void AddReservation(ReservationDto reservation);
 
         [OperationContract]
+        [FaultContract(typeof(DataManipulationFault))]
         void DeleteReservation(ReservationDto reservation);
 
         #endregion
