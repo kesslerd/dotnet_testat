862c6fa8d5185269fa1160296c953f91c7744b75
kesslerd <dominik@trueworld.ch>
2017-12-13 11:20:58 +0100

    add tests

---
+++ AutoReservation.UI.Testing/KundeTest.cs
@@ -0,0 +1,30 @@
+﻿using AutoReservation.TestEnvironment;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestStack.White;
+using TestStack.White.Factory;
+using TestStack.White.UIItems;
+
+namespace AutoReservation.UI.Testing
+{
+    [TestClass]
+    public class KundeTest : UITestBase
+    {
+
+        [TestMethod]
+        public void AddButtonOpensDialog()
+        {
+            var app = Application.Launch(SutPath);
+            var window = app.GetWindow("MainWindow", InitializeOption.NoCache);
+            Assert.IsNull(app.GetWindow("Kunde", InitializeOption.NoCache));
+            var button = window.Get<Button>("AddKundenButton");
+            button.Click();
+            Assert.IsNotNull(app.GetWindow("Kunde", InitializeOption.NoCache));
+            app.Close();
+        }
+    }
+}
